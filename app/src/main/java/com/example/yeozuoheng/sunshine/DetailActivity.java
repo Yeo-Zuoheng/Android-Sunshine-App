1fe412831dd7ac2af9c0b616c76203e5fc40d9cf
Yeo Zuoheng <yeo.zuoheng@gmail.com>
2016-04-06 18:33:39 +0800

    Chnge Action Bar to Tool Bar

--- app/src/main/java/com/example/yeozuoheng/sunshine/DetailActivity.java
+++ app/src/main/java/com/example/yeozuoheng/sunshine/DetailActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.support.v7.widget.Toolbar;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -12,6 +13,8 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         if (savedInstanceState == null) {
             // Create the detail fragment and add it to the activity
