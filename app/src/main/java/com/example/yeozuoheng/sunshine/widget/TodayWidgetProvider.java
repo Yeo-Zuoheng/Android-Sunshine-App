7e47b3410922f104f318a09256b24c3556334b47
NPStudent <NPStudent>
2016-08-25 21:37:06 +0800

    Getting real Data into the widget.

--- app/src/main/java/com/example/yeozuoheng/sunshine/widget/TodayWidgetProvider.java
+++ app/src/main/java/com/example/yeozuoheng/sunshine/widget/TodayWidgetProvider.java
@@ -1,45 +1,31 @@
 package com.example.yeozuoheng.sunshine.widget;
 
 
-import android.annotation.TargetApi;
-import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
-import android.widget.RemoteViews;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
 
-import com.example.yeozuoheng.sunshine.MainActivity;
-import com.example.yeozuoheng.sunshine.R;
-import com.example.yeozuoheng.sunshine.Utility;
+import com.example.yeozuoheng.sunshine.sync.SunshineSyncAdapter;
 
 public class TodayWidgetProvider extends AppWidgetProvider {
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        int weatherArtResourceId = R.drawable.art_clear;
-        String description = "Clear";
-        double maxTemp = 24;
-        String formattedMaxTemperature = Utility.formatTemperature(context, maxTemp);
-        for(int appWidgetId : appWidgetIds){
-            int layoutId = R.layout.widget_today_small;
-            RemoteViews views = new RemoteViews(context.getPackageName(), layoutId);
-            // Add the data to the RemoteViews
-            views.setImageViewResource(R.id.widget_icon, weatherArtResourceId);
-            // Content Descriptions for RemoteViews were only added in ICS MR1
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1) {
-                setRemoteContentDescription(views, description);
-            }
-            views.setTextViewText(R.id.widget_high_temperature, formattedMaxTemperature);
-            Intent launchIntent = new Intent(context, MainActivity.class);
-            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, launchIntent, 0);
-            views.setOnClickPendingIntent(R.id.widget, pendingIntent);
-
-            appWidgetManager.updateAppWidget(appWidgetId, views);
+        context.startService(new Intent(context, TodayWidgetIntentService.class));
         }
+
+    @Override
+    public void onAppWidgetOptionsChanged(Context context, AppWidgetManager appWidgetManager,
+                                          int appWidgetId, Bundle newOptions) {
+        context.startService(new Intent(context, TodayWidgetIntentService.class));
     }
-    @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1)
-    private void setRemoteContentDescription(RemoteViews views, String description) {
-        views.setContentDescription(R.id.widget_icon, description);
+    @Override
+    public void onReceive(@NonNull Context context,@NonNull Intent intent) {
+        super.onReceive(context, intent);
+        if(SunshineSyncAdapter.ACTION_DATA_UPDATED.equals(intent.getAction())){
+            context.startService(new Intent(context, TodayWidgetIntentService.class));
+        }
     }
 }
